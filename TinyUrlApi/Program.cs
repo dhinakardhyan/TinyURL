98e8e45728f397b712403fe340910f1dceb34dc3
dhinakarq <dhinakar.seetharaman@gmail.com>
2026-05-03 17:41:38 +0530

    Auto create SQLite database on startup

--- TinyUrlApi/Program.cs
+++ TinyUrlApi/Program.cs
@@ -51,4 +51,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
 app.Run();
